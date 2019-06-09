39c5519c74866fdd900a4b3d0db2c09978a96787
prevision <romanikani@gmail.com>
2019-06-09 15:29:07 +0300

    you

--- client/TrafficShare/main.lua
+++ client/TrafficShare/main.lua
@@ -12,7 +12,7 @@ local userClass = require("Classes.User")
 _G.User = userClass:new({name = "Роман", number = "2141 ",image = "Images/roma.png"})
 local composer = require("composer")
 
-composer.gotoScene("Scenes.Profile")
+composer.gotoScene("Scenes.Main")
 
 
 
